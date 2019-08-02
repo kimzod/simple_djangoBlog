5218b08be1de99587e410b682b749359384372bf
kimzod <kimzod4745@gmail.com>
2019-08-02 15:20:49 +0900

    url index 셋팅

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-created_date')
+    context = {"posts": posts}
+    return render(request, 'blog/index.html', context)
