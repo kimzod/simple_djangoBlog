1545b924a84351c38a35ab131e921cf77aa571cd
kimzod <kimzod4745@gmail.com>
2019-08-02 18:22:41 +0900

    CRUD 완성

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:pk>/', views.detail, name='detail'),
+    path('detail/<int:pk>/', views.detail, name='detail'),
+    path('add/', views.add, name='add'),
+    path('delete/<int:pk>/', views.delete, name='delete'),
 ]
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from blog.models import Post
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 
 def index(request):
-    posts = Post.objects.filter(published_date__isnull=False).order_by('-created_date')
+    posts = Post.objects.all().order_by('-created_date')
     context = {"posts": posts}
     return render(request, 'blog/index.html', context)
 
@@ -10,3 +12,23 @@ def detail(request, pk):
     post = Post.objects.get(pk=pk)
     context = {"post": post}
     return render(request, 'blog/detail.html', context)
+
+def add(request):
+    if request.method == 'POST':
+        User = get_user_model()
+        author = User.objects.get(username='zodlab')
+        title = request.POST['title']
+        content = request.POST['content']
+        post = Post.objects.create(author=author, title=title, content=content)
+        post_pk = post.pk
+        return redirect('blog:detail', pk=post_pk)
+    elif request.method == 'GET':
+        return render(request, 'blog/add.html')
+
+def delete(request, pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return render(request, 'blog/delete.html')
+    elif request.method == 'GET':
+        return HttpResponse('잘못된 접근입니다')
