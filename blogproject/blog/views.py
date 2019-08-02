1600433294efa06eaa7edcf2841d48bbb261e506
kimzod <kimzod4745@gmail.com>
2019-08-02 15:49:25 +0900

    static 설정 및 bootstrap 설치

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from blog.models import Post
 
 def index(request):
-    posts = Post.objects.all().order_by('-created_date')
+    posts = Post.objects.filter(published_date__isnull=False).order_by('-created_date')
     context = {"posts": posts}
     return render(request, 'blog/index.html', context)
